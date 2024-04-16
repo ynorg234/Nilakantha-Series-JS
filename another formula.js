622a5bf52ce33a751a4bc9a045ada0e75d2a360a
ynorg234 <119822347+ynorg234@users.noreply.github.com>
2024-04-16 19:38:28 -0400

    Create another formula.js

---
+++ another formula.js
@@ -0,0 +1,10 @@
+// Huge credits to BrolanJ for original code
+// Get code here: https://github.com/BrolanJ/Bailey-Borwein-Plouffe/blob/master/BBP%20Formula.py
+function Decimal(num) {return parseFloat(num);}
+function pi(iterations) {
+var pi = 0;
+  for (var k = 0; k <= iterations; k++) {
+    pi = Math.pow(1/Decimal(16), k) * (Decimal(4)/(8*k+1) - Decimal(2)/(8*k+4) - Decimal(1)/(8*k+5) - Decimal(1)/(8*k+5));
+  }
+  return pi;
+}
