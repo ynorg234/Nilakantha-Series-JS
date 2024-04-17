c9e2fed1b2d11ebf848a1b093354a0b8a7a5378a
ynorg234 <119822347+ynorg234@users.noreply.github.com>
2024-04-17 12:03:00 -0400

    Update another formula.js

--- another formula.js
+++ another formula.js
@@ -1,11 +1,18 @@
-// Huge credits to BrolanJ for original code
+// Huge credits to BrolanJ for og code
 // Get code here: https://github.com/BrolanJ/Bailey-Borwein-Plouffe/blob/master/BBP%20Formula.py
-// this code is broken pls fix
+// fixed after inspecting code more
 function Decimal(num) {return parseFloat(num);}
+function sum(arr) {
+  return arr.reduce(function(a, b) {return a+b;}, 0) // https://stackoverflow.com/questions/38203897/sum-function-in-javascript
+}
 function pi(iterations) {
 var pi = 0;
+var possible = Decimal(0);
+var arr = []
   for (var k = 0; k <= iterations; k++) {
-    pi = Math.pow(1/Decimal(16), k) * (Decimal(4)/(8*k+1) - Decimal(2)/(8*k+4) - Decimal(1)/(8*k+5) - Decimal(1)/(8*k+5));
+    possible = Decimal(Math.pow(1/Decimal(16), k) * (Decimal(4)/(8*k+1) - Decimal(2)/(8*k+4) - Decimal(1)/(8*k+5) - Decimal(1)/(8*k+5)));
+    arr.push(possible);
   }
+  pi = sum(arr);
   return pi;
 }
