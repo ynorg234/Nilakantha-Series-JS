1747dc9c5299828768d7b33aa5996dade4bad1fb
ynorg234 <119822347+ynorg234@users.noreply.github.com>
2024-04-17 12:05:36 -0400

    Update another formula.js

--- another formula.js
+++ another formula.js
@@ -12,6 +12,7 @@ var arr = []
   for (var k = 0; k <= iterations; k++) {
     possible = Decimal(Math.pow(1/Decimal(16), k) * (Decimal(4)/(8*k+1) - Decimal(2)/(8*k+4) - Decimal(1)/(8*k+5) - Decimal(1)/(8*k+5)));
     arr.push(possible);
+    console.log(arr);
   }
   pi = sum(arr);
   return pi;
